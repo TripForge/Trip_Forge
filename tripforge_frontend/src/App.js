452bc33981b7ce0892d3702066f1d2d6c2f7bf77
sidhant729 <sidhant1841.be21@chitkara.edu.in>
2024-02-22 20:57:34 +0530

    Adding User Profile Page

--- tripforge_frontend/src/App.js
+++ tripforge_frontend/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Starter from './Pages/Starter';
 import Hero from './/Pages/Hero';
+import UserProfile from './Components/user/UserProfile';
 
 function App() {
   return (
     <div className="App">
-      <Starter></Starter>
-      {/* <Hero></Hero> */}
+      {/* <Starter></Starter>
+      <Hero></Hero> */}
+      <UserProfile></UserProfile>
     </div>
   );
 }
