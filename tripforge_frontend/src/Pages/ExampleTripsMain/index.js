113274012e682840d38c2f33225a6a4c37b3c9c1
sidhant729 <sidhant1841.be21@chitkara.edu.in>
2024-02-22 16:48:02 +0530

    pulling

---
+++ tripforge_frontend/src/Pages/ExampleTripsMain/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ExampleTrips from '../ExampleTrips/index'
+const trips :
+function index() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default index
