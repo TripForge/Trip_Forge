d16674cf386def67ae9b9b61c776b3b713fee903
sidhant729 <sidhant1841.be21@chitkara.edu.in>
2024-02-27 16:28:09 +0530

    adding some changes

--- tripforge_frontend/src/App.js
+++ tripforge_frontend/src/App.js
@@ -2,11 +2,11 @@ import './App.css';
 import Starter from './Pages/Starter';
 import Hero from './/Pages/Hero';
 import UserProfile from './Components/user/UserProfile';
-
+import PastTrips from './Components/PastTrips/components';
 function App() {
   return (
     <div className="App">
-      <Hero></Hero>
+     <PastTrips></PastTrips>
     </div>
   );
 }
