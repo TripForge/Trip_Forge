87f165d06219b38407dd056a523231cb83f14b92
sidhant729 <sidhant1841.be21@chitkara.edu.in>
2024-03-09 21:46:27 +0530

    created user api

--- tripforge_backend/index.js
+++ tripforge_backend/index.js
@@ -2,25 +2,37 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const app = express();
-const userRouter = require('./routes/Auth');
-// const signRoute = require("./routes/Auth");
+const userRouter = require("./routes/Auth");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+
 dotenv.config();
-
-
-app.use('/users', userRouter.router);
-mongoose.connect(process.env.MONGO_URI)
-.then(() => {
-    console.log('Connected to MongoDB successfully');
-})
-.catch(error => {
-    console.error('Error connecting to MongoDB:', error);
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
 });
+
+app.use("/users", userRouter);
+app.use("/fetchUser", userRouter);
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Connected to MongoDB successfully");
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+  });
 
 const PORT = process.env.PORT;
 
 app.listen(PORT || 4000, () => {
-    console.log(`Server started at ${PORT}`);
-  });
+  console.log(`Server started at ${PORT}`);
+});
 
 app.use(express.json());
 // app.use(cors());
