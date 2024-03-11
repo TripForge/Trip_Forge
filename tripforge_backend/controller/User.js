05b450ea7fca005fe6d67c4df46b4742606cc462
sidhant729 <sidhant1841.be21@chitkara.edu.in>
2024-03-11 21:07:24 +0530

    userProfile backend and frontend connected and check data is sent or not

--- tripforge_backend/controller/User.js
+++ tripforge_backend/controller/User.js
@@ -3,6 +3,7 @@ module.exports.createUserInfo = async (req, res, next) => {
     const userInfo =  new UserInfo(req.body);
     try {
         const doc = await userInfo.save();
+        console.log('doc is', doc);
         res.status(201).json(doc);
     } catch (err) {
         res.status(400).json({ error: err.message });
