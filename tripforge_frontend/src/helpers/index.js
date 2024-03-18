8746ce11c1a362fb3e23847288385b6ee559532f
aman1348 <asinghchuphal@gmail.com>
2024-03-18 20:43:56 +0530

    completed edit uder profile page

--- tripforge_frontend/src/helpers/index.js
+++ tripforge_frontend/src/helpers/index.js
@@ -3,9 +3,10 @@ export const createNewArray = (size, val) => Array(size).fill(val);
 export const days = (startDate, endDate) => ((endDate?.getTime() - startDate?.getTime()) / (1000 * 3600 * 24));
 
 export const dateString = (date) => {
-    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-    let myDate = `${date.getDate()} ${months[date.getMonth()]} ${date.getFullYear()}`;
-    return myDate;
+	if (!date) return "";
+	const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+	let myDate = `${date.getDate()} ${months[date.getMonth()]} ${date.getFullYear()}`;
+	return myDate;
 };
 
 
